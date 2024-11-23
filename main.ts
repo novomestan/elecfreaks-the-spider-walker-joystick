38140547ca49a1116d1f1a140787cf2908e802bb
novomestan <78145236+novomestan@users.noreply.github.com>
2024-11-23 17:00:43 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,11 @@
-
+joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P14, joystickbit.ButtonType.down, function () {
+    radio.sendNumber(2)
+})
+joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P15, joystickbit.ButtonType.down, function () {
+    radio.sendNumber(3)
+})
+joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P13, joystickbit.ButtonType.down, function () {
+    radio.sendNumber(1)
+})
+radio.setGroup(1)
+basic.showIcon(IconNames.Angry)
